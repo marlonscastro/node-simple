109f4404a952a00029cef7044af0866bb599748a
marlonscastro <marloncastro@live.com>
2020-04-04 11:22:45 -0300

    ...

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const axios = require('axios');
+// const axios = require('axios');
 
-const api = axios.create({
-    baseURL: 'http://10.238.83.84/bopmpe/api'
-})
+const PORT = process.env.PORT || 8080;
 
-const timeout = ms => new Promise(res => setTimeout(res, ms))
+// const api = axios.create({
+//     baseURL: 'http://10.238.83.84/bopmpe/api'
+// })
+
 
 const app = express();
 
@@ -18,18 +19,9 @@ function getRandomIntInclusive(min, max) {
 const num = getRandomIntInclusive(1, 10);
 
 app.get('/', async (req, res) => {
-
-    let response = await api.get('/unidades', {
-        headers: {
-            Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsIm1hdHJpY3VsYSI6MTEzNzIwNCwibmFtZSI6Ik1hcmxvbiBkZSBMaW1hIENhc3RybyIsImdyYWR1YWNhbyI6IlNvbGRhZG8iLCJleHBpcmVkX2F0IjoiMjAyMC0wMi0yMiAwOS0xMC0wNCJ9.A4ZNbaXjrQx8er7_LkesKSCv9zFwFdsTWUIOyygD92w`
-        }
-    });
-    console.log(response.data);
-
     return res.status(200).json({
-        'Server id': num,
-        'dados 10.238.83.84': response.data
+        'Server id': num
     });
 });
 
-app.listen(8080);
+app.listen(PORT);
