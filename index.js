a052b6c59ea04847ce0bcf231f8cf4ad0b77abce
Marlon Castro <marlon.castro@pm.pe.gov.br>
2020-04-07 22:08:34 -0300

    alguma modificacao

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const express = require('express');
-// const axios = require('axios');
 
 const PORT = process.env.PORT || 8080;
-
-// const api = axios.create({
-//     baseURL: 'http://10.238.83.84/bopmpe/api'
-// })
-
 
 const app = express();
 
@@ -21,7 +15,7 @@ const num = getRandomIntInclusive(1, 10);
 app.get('/', async (req, res) => {
     return res.status(200).json({
         'Server id': num,
-        'novoItem': 'atualização do sistema v2'
+        'message': 'uma mensagem qualquer'
     });
 });
 
